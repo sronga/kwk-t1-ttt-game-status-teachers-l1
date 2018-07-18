adbac4e936baad2288dc04b436219f0e24bef29e
sronga <sr899@georgetown.edu>
2018-07-18 15:34:29 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,11 +29,11 @@ def won(board)
 
   end
 
-puts "hi"
 
-##  if playerposition1 == "X" && playerposition2 == "X" && playerposition3 == "X"
-##    WIN_COMBINATIONS[winindex]
-##  elsif playerposition1 == "O" && playerposition2 == "O" && playerposition3 == "O"
-##    WIN_COMBINATIONS[winindex]
-##  else false
+  if playerposition1 == "X" && playerposition2 == "X" && playerposition3 == "X"
+    WIN_COMBINATIONS[winindex]
+  elsif playerposition1 == "O" && playerposition2 == "O" && playerposition3 == "O"
+    WIN_COMBINATIONS[winindex]
+  else false
+  end
 end
