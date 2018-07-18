0aba183bdaf8df9e60695211dc57c342b359be42
sronga <sr899@georgetown.edu>
2018-07-18 15:26:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,5 +26,10 @@ def won(board)
   playerposition2 = board[position2]
   playerposition3 = board[position3]
 
+  if playerposition1 == "X" && playerposition2 == "X" && playerposition3 == "X"
+    WIN_COMBINATIONS[winindex]
+  elsif playerposition1 == "O" && playerposition2 == "O" && playerposition3 == "O"
+    WIN_COMBINATIONS[winindex]
+  else nil
   end
 end
